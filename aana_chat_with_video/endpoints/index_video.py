f9375c0a6d8ec414d650a0e8706df60401360076
Hossein Rashidi <h.rashidi128@gmail.com>
2024-10-31 07:55:51 +0000

    add new model and fix sessions

--- aana_chat_with_video/endpoints/index_video.py
+++ aana_chat_with_video/endpoints/index_video.py
@@ -1,6 +1,7 @@
 from collections.abc import AsyncGenerator
 from typing import TYPE_CHECKING, Annotated, TypedDict
-
+import asyncio
+from aana.storage.session import get_session
 from pydantic import Field
 
 from aana.api.api_generation import Endpoint
@@ -13,6 +14,8 @@ from aana.core.models.media import MediaId
 from aana.core.models.vad import VadParams
 from aana.core.models.video import VideoInput, VideoMetadata, VideoParams
 from aana.core.models.whisper import BatchedWhisperParams
+from aana.core.models.image_chat import ImageChatDialog
+
 from aana.deployments.aana_deployment_handle import AanaDeploymentHandle
 from aana.exceptions.db import MediaIdAlreadyExistsException
 from aana.exceptions.io import VideoTooLongException
@@ -66,9 +69,6 @@ class IndexVideoEndpoint(Endpoint):
         self.captioning_handle = await AanaDeploymentHandle.create(
             "captioning_deployment"
         )
-        self.extended_video_repo = ExtendedVideoRepository(self.session)
-        self.transcript_repo = ExtendedVideoTranscriptRepository(self.session)
-        self.caption_repo = ExtendedVideoCaptionRepository(self.session)
 
     async def run(  # noqa: C901
         self,
@@ -79,122 +79,131 @@ class IndexVideoEndpoint(Endpoint):
     ) -> AsyncGenerator[IndexVideoOutput, None]:
         """Transcribe video in chunks."""
         media_id = video.media_id
-        if self.extended_video_repo.check_media_exists(media_id):
-            raise MediaIdAlreadyExistsException(table_name="media", media_id=video)
+        with get_session() as session:
+            extended_video_repo = ExtendedVideoRepository(session)
+            transcript_repo = ExtendedVideoTranscriptRepository(session)
+            caption_repo = ExtendedVideoCaptionRepository(session)
+            if extended_video_repo.check_media_exists(media_id):
+                raise MediaIdAlreadyExistsException(table_name="media", media_id=video)
 
-        video_duration = None
-        if video.url is not None:
-            video_metadata = get_video_metadata(video.url)
-            video_duration = video_metadata.duration
+            video_duration = None
+            if video.url is not None:
+                video_metadata = get_video_metadata(video.url)
+                video_duration = video_metadata.duration
 
-        # precheck for max video length before actually download the video if possible
-        if video_duration and video_duration > settings.max_video_len:
-            raise VideoTooLongException(
-                video=video,
-                video_len=video_duration,
-                max_len=settings.max_video_len,
-            )
-
-        video_obj: Video = await run_remote(download_video)(video_input=video)
-        if video_duration is None:
-            video_duration = await run_remote(get_video_duration)(video=video_obj)
-
-        if video_duration > settings.max_video_len:
-            raise VideoTooLongException(
-                video=video_obj,
-                video_len=video_duration,
-                max_len=settings.max_video_len,
-            )
-
-        self.extended_video_repo.save(video=video_obj, duration=video_duration)
-        yield {
-            "media_id": media_id,
-            "metadata": VideoMetadata(
-                title=video_obj.title,
-                description=video_obj.description,
-                duration=video_duration,
-            ),
-        }
-
-        try:
-            self.extended_video_repo.update_status(
-                media_id, VideoProcessingStatus.RUNNING
-            )
-            audio: Audio = extract_audio(video=video_obj)
-
-            # TODO: Update once batched whisper PR is merged
-            # vad_output = await self.vad_handle.asr_preprocess_vad(
-            #     audio=audio, params=vad_params
-            # )
-            # vad_segments = vad_output["segments"]
-
-            transcription_list = []
-            segments_list = []
-            transcription_info_list = []
-            async for whisper_output in self.asr_handle.transcribe_stream(
-                audio=audio, params=whisper_params
-            ):
-                transcription_list.append(whisper_output["transcription"])
-                segments_list.append(whisper_output["segments"])
-                transcription_info_list.append(whisper_output["transcription_info"])
-                yield {
-                    "transcription": whisper_output["transcription"],
-                    "segments": whisper_output["segments"],
-                    "transcription_info": whisper_output["transcription_info"],
-                }
-            transcription = sum(transcription_list, AsrTranscription())
-            segments = sum(segments_list, AsrSegments())
-            transcription_info = sum(transcription_info_list, AsrTranscriptionInfo())
-
-            captions = []
-            timestamps = []
-            frame_ids = []
-
-            async for frames_dict in run_remote(generate_frames)(
-                video=video_obj, params=video_params
-            ):
-                if len(frames_dict["frames"]) == 0:
-                    break
-
-                timestamps.extend(frames_dict["timestamps"])
-                frame_ids.extend(frames_dict["frame_ids"])
-
-                captioning_output = await self.captioning_handle.generate_batch(
-                    images=frames_dict["frames"]
+            # precheck for max video length before actually download the video if possible
+            if video_duration and video_duration > settings.max_video_len:
+                raise VideoTooLongException(
+                    video=video,
+                    video_len=video_duration,
+                    max_len=settings.max_video_len,
                 )
-                captions.extend(captioning_output["captions"])
+
+            video_obj: Video = await run_remote(download_video)(video_input=video)
+            if video_duration is None:
+                video_duration = await run_remote(get_video_duration)(video=video_obj)
+
+            if video_duration > settings.max_video_len:
+                raise VideoTooLongException(
+                    video=video_obj,
+                    video_len=video_duration,
+                    max_len=settings.max_video_len,
+                )
+
+            extended_video_repo.save(video=video_obj, duration=video_duration)
+            yield {
+                "media_id": media_id,
+                "metadata": VideoMetadata(
+                    title=video_obj.title,
+                    description=video_obj.description,
+                    duration=video_duration,
+                ),
+            }
+
+            try:
+                extended_video_repo.update_status(
+                    media_id, VideoProcessingStatus.RUNNING
+                )
+                audio: Audio = extract_audio(video=video_obj)
+
+                # TODO: Update once batched whisper PR is merged
+                # vad_output = await self.vad_handle.asr_preprocess_vad(
+                #     audio=audio, params=vad_params
+                # )
+                # vad_segments = vad_output["segments"]
+
+                transcription_list = []
+                segments_list = []
+                transcription_info_list = []
+                async for whisper_output in self.asr_handle.transcribe_stream(
+                    audio=audio, params=whisper_params
+                ):
+                    transcription_list.append(whisper_output["transcription"])
+                    segments_list.append(whisper_output["segments"])
+                    transcription_info_list.append(whisper_output["transcription_info"])
+                    yield {
+                        "transcription": whisper_output["transcription"],
+                        "segments": whisper_output["segments"],
+                        "transcription_info": whisper_output["transcription_info"],
+                    }
+                transcription = sum(transcription_list, AsrTranscription())
+                segments = sum(segments_list, AsrSegments())
+                transcription_info = sum(transcription_info_list, AsrTranscriptionInfo())
+
+                captions = []
+                timestamps = []
+                frame_ids = []
+
+                async for frames_dict in run_remote(generate_frames)(
+                    video=video_obj, params=video_params
+                ):
+                    if len(frames_dict["frames"]) == 0:
+                        break
+
+                    timestamps.extend(frames_dict["timestamps"])
+                    frame_ids.extend(frames_dict["frame_ids"])
+                    chat_prompt = "Describe the content of the following image in a single sentence:"
+                    dialogs = [
+                        ImageChatDialog.from_prompt(prompt=chat_prompt, images=[frame]) for frame in frames_dict["frames"]
+                    ]
+
+                    # Collect the tasks to run concurrently and wait for them to finish
+                    tasks = [self.captioning_handle.chat(dialog) for dialog in dialogs]
+                    captioning_output = await asyncio.gather(*tasks)
+                    captioning_output = [caption["message"].content for caption in captioning_output]
+                    captions.extend(captioning_output)
+
+                    yield {
+                        "captions": captioning_output,
+                        "timestamps": frames_dict["timestamps"],
+                    }
+
+                transcription_entity = transcript_repo.save(
+                    model_name=settings.asr_model_name,
+                    media_id=video_obj.media_id,
+                    transcription=transcription,
+                    segments=segments,
+                    transcription_info=transcription_info,
+                )
+
+                caption_entities = caption_repo.save_all(
+                    model_name=settings.captioning_model_name,
+                    media_id=video_obj.media_id,
+                    captions=captions,
+                    timestamps=timestamps,
+                    frame_ids=frame_ids,
+                )
 
                 yield {
-                    "captions": captioning_output["captions"],
-                    "timestamps": frames_dict["timestamps"],
+                    "transcription_id": transcription_entity.id,
+                    "caption_ids": [c.id for c in caption_entities],
                 }
-
-            transcription_entity = self.transcript_repo.save(
-                model_name=settings.asr_model_name,
-                media_id=video_obj.media_id,
-                transcription=transcription,
-                segments=segments,
-                transcription_info=transcription_info,
-            )
-
-            caption_entities = self.caption_repo.save_all(
-                model_name=settings.captioning_model_name,
-                media_id=video_obj.media_id,
-                captions=captions,
-                timestamps=timestamps,
-                frame_ids=frame_ids,
-            )
-
-            yield {
-                "transcription_id": transcription_entity.id,
-                "caption_ids": [c.id for c in caption_entities],
-            }
-        except BaseException:
-            self.extended_video_repo.update_status(
-                media_id, VideoProcessingStatus.FAILED
-            )
-            raise
-        else:
-            self.extended_video_repo.update_status(
-                media_id, VideoProcessingStatus.COMPLETED
-            )
+            except BaseException:
+                extended_video_repo.update_status(
+                    media_id, VideoProcessingStatus.FAILED
+                )
+                raise
+            else:
+                extended_video_repo.update_status(
+                    media_id, VideoProcessingStatus.COMPLETED
+                )
